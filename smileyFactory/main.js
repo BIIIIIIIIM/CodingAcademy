4373d782d69cd97f59e9ddbf2047c29682d4c4c4
GREG WEGWEBandGOGO <WEBandGO@MG.local>
2019-05-21 15:04:57 +0200

    final push

---
+++ smileyFactory/main.js
@@ -0,0 +1,121 @@
+
+
+function loveFunction(){
+const emobody=document.getElementsByClassName("emobody")
+var tl = new TimelineLite();
+
+
+/*TweenMax.to(".emobody",2,{margin:"70%"});*/
+
+tl.to(".emobody", 1.6, {top: '-100%', y: '410%', ease:Bounce.easeOut, delay: 2});
+
+tl.to(".emobody", 1.6, {x: '135px', ease:Back.easeInOut, delay: 2});
+
+tl.to(".emobody", 1.6, {x: '270px', ease:Back.easeInOut, delay: 4});
+tl.to(".emobody", 1.6, {x: '-400px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:4});
+
+
+const love1=document.getElementsByClassName("love1")
+var tl1 = new TimelineLite();
+
+tl1.to(".love1", 1.6, {top: '-100%', y: '450%', ease:Bounce.easeOut, delay: 7.6});
+
+tl1.to(".love1", 2.3, {x: '135px', rotation: 1080, ease:Bounce.easeOut, delay: 2.247});
+tl1.to(".love1", 1.9, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:3.05});
+
+const love2=document.getElementsByClassName("love2")
+var tl2 = new TimelineLite();
+
+tl2.to(".love2", 1.6, {top: '-100%', y: '740%', ease:Bounce.easeOut, delay: 13.2});
+tl2.to(".love2", 1.1, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false),delay:2});
+
+}
+
+
+
+
+function sadeFunction(){
+    const emobody=document.getElementsByClassName("emobody")
+    var tl3 = new TimelineLite();
+    
+    /*TweenMax.to(".emobody",2,{margin:"70%"});*/
+    
+    tl3.to(".emobody", 1.6, {top: '-100%', y: '410%', ease:Bounce.easeOut, delay: 2});
+    
+    tl3.to(".emobody", 1.6, {x: '135px', ease:Back.easeInOut, delay: 2});
+    
+    tl3.to(".emobody", 1.6, {x: '270px', ease:Back.easeInOut, delay: 4});
+    tl3.to(".emobody", 1.6, {x: '-400px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:4});
+    
+    const tongue1=document.getElementsByClassName("sade1")
+    var tl4 = new TimelineLite();
+    
+    tl4.to(".sade1", 1.6, {top: '-100%', y: '465%', ease:Bounce.easeOut, delay: 7.6});
+    
+    tl4.to(".sade1", 2.3, {x: '135px', rotation: 1080, ease:Bounce.easeOut, delay: 2.247});
+    tl4.to(".sade1", 1.9, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:3.05});
+    
+    const tongue2=document.getElementsByClassName("sade2")
+    var tl5 = new TimelineLite();
+    
+    tl5.to(".sade2", 1.6, {top: '-100%', y: '740%', ease:Bounce.easeOut, delay: 13.2});
+    tl5.to(".sade2", 1.1, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false),delay:2});
+
+}
+function tongueFunction(){
+    const emobody=document.getElementsByClassName("emobody")
+    var tl6 = new TimelineLite();
+    
+    /*TweenMax.to(".emobody",2,{margin:"70%"});*/
+    
+    tl6.to(".emobody", 1.6, {top: '-100%', y: '410%', ease:Bounce.easeOut, delay: 2});
+    
+    tl6.to(".emobody", 1.6, {x: '135px', ease:Back.easeInOut, delay: 2});
+    
+    tl6.to(".emobody", 1.6, {x: '270px', ease:Back.easeInOut, delay: 4});
+    tl6.to(".emobody", 1.6, {x: '-400px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:4});
+    
+    const tongue1=document.getElementsByClassName("tongue1")
+    var tl7 = new TimelineLite();
+    
+    tl7.to(".tongue1", 1.6, {top: '-100%', y: '460%', ease:Bounce.easeOut, delay: 7.6});
+    
+    tl7.to(".tongue1", 2.3, {x: '135px', rotation: 1080, ease:Bounce.easeOut, delay: 2.247});
+    tl7.to(".tongue1", 1.9, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:3.05});
+    
+    const tongue2=document.getElementsByClassName("tongue2")
+    var tl8 = new TimelineLite();
+    
+    tl8.to(".tongue2", 1.6, {top: '-100%', y: '747%', ease:Bounce.easeOut, delay: 13.2});
+    tl8.to(".tongue2", 1.1, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false),delay:2});
+
+}
+
+function happyFunction(){
+    const emobody=document.getElementsByClassName("emobody")
+    var tl9 = new TimelineLite();
+    
+    /*TweenMax.to(".emobody",2,{margin:"70%"});*/
+    
+    tl9.to(".emobody", 1.6, {top: '-100%', y: '410%', ease:Bounce.easeOut, delay: 2});
+    
+    tl9.to(".emobody", 1.6, {x: '135px', ease:Back.easeInOut, delay: 2});
+    
+    tl9.to(".emobody", 1.6, {x: '270px', ease:Back.easeInOut, delay: 4});
+    tl9.to(".emobody", 1.6, {x: '-400px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:4});
+    
+    const happy1=document.getElementsByClassName("happy1")
+    var tl10 = new TimelineLite();
+    
+    tl10.to(".happy1", 1.6, {top: '-100%', y: '470%', ease:Bounce.easeOut, delay: 7.6});
+    
+    tl10.to(".happy1", 2.3, {x: '135px', rotation: 1080, ease:Bounce.easeOut, delay: 2.247});
+    tl10.to(".happy1", 1.9, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false), delay:3.05});
+    
+    const happy2=document.getElementsByClassName("happy2")
+    var tl11 = new TimelineLite();
+    
+    tl11.to(".happy2", 1.6, {top: '-100%', y: '740%', ease:Bounce.easeOut, delay: 13.2});
+    tl11.to(".happy2", 1.1, {x: '-535px', rotation: -2080, ease: SlowMo.ease.config(0.1, 0.7, false),delay:2});
+
+}
